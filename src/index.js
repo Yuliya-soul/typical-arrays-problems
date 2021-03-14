5b4aa481df1c99e762b27afc324324333c408a41
Your Name <darki@tut.by>
2021-03-14 23:18:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+if((array.length===0)||(array===undefined)) {return 0} 
+else{return Math.min.apply(null, array)}
+
+ }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  for( var i = 0; i < array.length; i++ ){
+      sum += parseInt( array[i], 10 ); 
+  }
+  
+ return sum/array.length;
+  
+  
 }
